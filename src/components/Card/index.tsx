4eab4149bf7fa043f3950efbcfb24e2e1b311acb
liniker <linikers@hotmail.com>
2023-04-05 17:58:07 -0300

    atualização botão card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import Image from "next/image";
 import styles from "@/styles/Card.module.css";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { FavoriteContext, iCard } from "@/context";
 
@@ -10,6 +10,15 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Card({ id, name, image, specie }: iCard) {
   const { setFavorites, favorites, addToFavorites } =
     useContext(FavoriteContext);
+
+  const [isFavorite, setFavorite] = useState(false);
+  useEffect(() => {
+    if (favorites.some((fav) => fav.id === id)) {
+      setFavorite(true);
+    } else {
+      setFavorite(false);
+    }
+  }, [favorites]);
 
   return (
     <li key={id} className={styles.card}>
@@ -24,17 +33,23 @@ export default function Card({ id, name, image, specie }: iCard) {
       )}
       <h2>{name}</h2>
       <p>{specie}</p>
-      <FavoriteBtn card={{ id, name, image, specie }} />
+      <FavoriteBtn card={{ id, name, image, specie }} isFavorite={isFavorite} />
     </li>
   );
 }
 
-function FavoriteBtn({ card }: { card: iCard }) {
+function FavoriteBtn({
+  card,
+  isFavorite,
+}: {
+  card: iCard;
+  isFavorite: boolean;
+}) {
   const { favorites, addToFavorites } = useContext(FavoriteContext);
 
   return (
     <button className={styles.btnCard} onClick={() => addToFavorites(card)}>
-      Favoritos
+      {isFavorite ? "Remover" : "Favoritos"}
     </button>
   );
 }
